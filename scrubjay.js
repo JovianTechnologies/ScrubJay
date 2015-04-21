23673b0de89574fbff637e4e684c71543348a5a4
Eric Smallwood <smallwood.eric@ymail.com>
2015-04-21 08:15:18 -0500

    - moved header parsing into its own function - added another todo

--- scrubjay.js
+++ scrubjay.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
     //todo: Hide all elements when initially loaded so there is no flashing of page data
+    //todo: remove all hardcoded strings
+    //todo: replace string comparisons with regular expressions
+    //todo: decide whether or not to implement without self loading
 
     //get sectons to be inserted into template
     var insertions = document.querySelectorAll('[sj-section]');
@@ -14,19 +16,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     var masterPage = document.body.firstChild.data;
-    var url = parseFileName(masterPage);
+    var url = parseMasterFileName(masterPage);
 
     //set up page
     getPage(url, function(pageData){
-        //Copy body only and capture head data, which will be added after the body is set
-        var beginHeadStartTagLocation = pageData.indexOf("<head");
-        var tempPageData = pageData.substring(beginHeadStartTagLocation);
-        var endHeadStartTagLocation = tempPageData.indexOf(">");
-        var pageDataNoHead = pageData.substring(0, beginHeadStartTagLocation + endHeadStartTagLocation + 1) + pageData.substring(pageData.indexOf("</head>"));
-        var pageHead = pageData.substring(beginHeadStartTagLocation + endHeadStartTagLocation + 6, pageData.indexOf("</head>"));
+        var page = parseMasterFileHead(pageData);
 
-        document.body.innerHTML = pageDataNoHead;
-        document.head.innerHTML = pageHead;
+        document.body.innerHTML = page.body;
+        document.head.innerHTML = page.head;;
 
         //place section data into template
         var sections = document.querySelectorAll('[sj-insert]');
@@ -44,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function getPage(url, callback){
         var request = new XMLHttpRequest();
         request.open("GET", url, true);
-        request.setRequestHeader("Accept", "document");
+        request.setRequestHeader("Accept", "text/html");
         request.onreadystatechange = function(){
             if(request.readyState == 4 && request.status == 200)
                 callback(request.response);
@@ -52,7 +49,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
         request.send(null);
     }
 
-    function parseFileName(requestedFile){
-        return requestedFile.substring(requestedFile.indexOf('("') + 2, requestedFile.indexOf('")'));
+    function parseMasterFileName(file){
+        var useDirecive = "@sj-use";
+        return file.substring(file.indexOf(useDirecive) + useDirecive.length + 2/*add two to take into the ( and " charcters into account */, file.indexOf('")'));
     }
+
+    function parseMasterFileHead(file){
+        //Copy body only and capture head data, which will be added after the body is set
+        var beginHeadStartTagLocation = file.indexOf("<head");
+        var tempPageData = file.substring(beginHeadStartTagLocation);
+        var endHeadStartTagLocation = tempPageData.indexOf(">");
+        var pageBody = file.substring(0, beginHeadStartTagLocation + endHeadStartTagLocation + 1) + file.substring(file.indexOf("</head>"));
+        var pageHead = file.substring(beginHeadStartTagLocation + endHeadStartTagLocation + 6, file.indexOf("</head>"));
+
+        return new MasterPage(pageHead, pageBody);
+    }
+
+    var MasterPage = function(head, body){
+        this.head = head;
+        this.body = body;
+    };
 });
